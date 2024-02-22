a71ddd7a1c30dae0c851b23039b58bc76807e9f7
William Downey <wdowney20@gmail.com>
2024-02-22 17:49:32 -0500

    implement terminal scrolling

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -38,14 +38,42 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
+void terminal_scroll(int line) {
+    int loop;
+    char c;
+
+    for(loop = line * (VGA_WIDTH * 2) + 0xB8000; loop < VGA_WIDTH * 2; loop++) {
+        c = *loop;
+        *(loop - (VGA_WIDTH * 2)) = c;
+    }
+}
+
+void terminal_delete_last_line() {
+    int x, *ptr;
+
+    for(x = 0; x < VGA_WIDTH * 2; x++) {
+        ptr = 0xB8000 + (VGA_WIDTH * 2) * (VGA_HEIGHT - 1) + x;
+        *ptr = 0;
+    }
+}
+
 void terminal_putchar(char c) {
-	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+    int line;
+    unsigned char uc = c;
+
+    terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+        terminal_column = 0;
+        if (++terminal_row == VGA_HEIGHT)
+        {
+            for(line = 1; line <= VGA_HEIGHT - 1; line++)
+            {
+                terminal_scroll(line);
+            }
+            terminal_delete_last_line();
+            terminal_row = VGA_HEIGHT - 1;
+        }
+    }
 }
 
 void terminal_write(const char* data, size_t size) {
